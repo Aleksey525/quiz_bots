7087092913d40eb5e38109d071bfe2b7ed112a28
Aleksey525 <alex_tolchin@mail.ru>
2024-09-15 16:00:12 +0300

    connected the database and added handlers

--- dict_create.py
+++ dict_create.py
@@ -1,6 +1,7 @@
 def create_dict_with_questions():
     with open('quiz-questions/12koll07.txt', 'r', encoding='KOI8-R') as file:
         file_content = file.read()
+    print(file_content)
     questions_and_answers = {}
     sections = file_content.split('\n\n')
     question = None
@@ -10,15 +11,16 @@ def create_dict_with_questions():
             part = ' '.join(part.split()[2:])
             question = part
         elif part.startswith('Ответ'):
-            part = part.replace('\n', ' ').lstrip('Ответ:')
+            part = part.replace('\n', ' ').lstrip(' Ответ:').rstrip('.')
             answer = part
         if question and answer:
             questions_and_answers[question] = answer
+    print(questions_and_answers)
     return questions_and_answers
 
 
 
-
+create_dict_with_questions()
 
 
 
--- tg_bot.py
+++ tg_bot.py
@@ -1,6 +1,7 @@
 import random
 import telegram
 from environs import Env
+import redis
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
@@ -22,11 +23,30 @@ def start(update: Update, context: CallbackContext):
                              reply_markup=reply_markup)
 
 
-def make_question(update: Update, context: CallbackContext) -> None:
+def handle_new_question_request(update: Update, context: CallbackContext):
     dict_with_questions = create_dict_with_questions()
-    if update.message.text == 'Новый вопрос':
-        question, answer = random.choice(list(dict_with_questions.items()))
-        update.message.reply_text(question)
+    chat_id = update.message.chat_id
+    r = redis.Redis(host='redis-12998.c299.asia-northeast1-1.gce.redns.redis-cloud.com', port=12998,
+                    password='tzo2yKYPlXlqsGTkZA4IPKTfOvoBuSl1', db=0, decode_responses=True)
+    question, answer = random.choice(list(dict_with_questions.items()))
+    r.set(str(chat_id), question)
+    question = r.get(str(chat_id))
+    update.message.reply_text(question)
+    answer = dict_with_questions[question]
+    payload = {'answer': answer}
+    context.bot_data.update(payload)
+    print(context.bot_data)
+
+
+def handle_solution_attempt(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    if 'answer' in context.bot_data and update.message.text == context.bot_data['answer']:
+        context.bot.send_message(chat_id=chat_id,
+                                 text='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос')
+        del context.bot_data['answer']  # Clear the answer after checking
+    else:
+        context.bot.send_message(chat_id=chat_id,
+                                 text='Неправильно… Попробуешь ещё раз?')
 
 
 def main():
@@ -36,7 +56,8 @@ def main():
     updater = Updater(bot_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.regex(r'^Новый вопрос$'), handle_new_question_request))
+    dispatcher.add_handler(MessageHandler(Filters.update.message & Filters.text, handle_solution_attempt))
     updater.start_polling()
     updater.idle()
 
